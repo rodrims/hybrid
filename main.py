6d2a2afd523803f25369ae5b351fbd4e189b1893
Rodrigo Mejia <rodri.m.sanchez@gmail.com>
2018-11-04 12:45:10 -0500

    refactor to oo

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import pygame
+from pygame.locals import *
 
-def main():
-	pygame.init()
-	logo = pygame.image.load("assets/chess/white_rook.png")
-	pygame.display.set_icon(logo)
-	pygame.display.set_caption("chess")
+class App:
+	def __init__(self):
+		self.running = True
+		self.display_surf = None
+		self.size = self.weight, self.height = 640, 400
 
-	screen = pygame.display.set_mode((240, 180))
+	def on_init(self):
+		pygame.init()
 
-	running = True
+		logo = pygame.image.load("assets/chess/white_rook.png")
+		pygame.display.set_icon(logo)
+		pygame.display.set_caption("chess")
+		self.display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
+		self.running = True
+
+		return True
+	
+	def on_event(self, event):
+		if event.type == pygame.QUIT:
+			self.running = False
+
+	def on_loop(self):
+		pass
+
+	def on_render(self):
+		pass
+
+	def on_cleanup(self):
+		pygame.quit()
+
+	def on_execute(self):
+		if not self.on_init(): #TODO: understand how this works
+			self.running = False
+
+		while self.running:
+			for event in pygame.event.get():
+				self.on_event(event)
+
+			self.on_loop()
+			self.on_render()
+
+		self.on_cleanup()
 
 if __name__ == "__main__":
-	main()
+	app = App()
+	app.on_execute()
